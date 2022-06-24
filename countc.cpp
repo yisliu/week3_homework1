68d7a5ed9ba7ba015ca892b8055809d3a953b0a0
Yisi Liu <85250232+yisliu@users.noreply.github.com>
2022-06-24 20:01:59 +0000

    Initial commit

---
+++ countc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  //make variables for input
+  string str1;
+  int num=0;
+  int letter=0;
+  int other=0;
+  cin>>str1;
+  //make for loop for counting the different types of characters
+  for(int i = 0; i<str1.length(); i++){
+    if(str1[i]>='0' and str1[i]<='9'){
+      num+=1;
+    }
+    else if(str1[i]>='a' and str1[i]<='z'){
+      letter+=1;
+    }
+    else if(str1[i]>='A' and str1[i]<='Z'){
+      letter+=1;
+    }
+    else{
+      other++;
+    }
+  }
+  //output
+  cout<<letter<<" "<<num<<" "<<other;
+}
